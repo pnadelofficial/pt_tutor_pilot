e9dbecbddfcec04ad9a5a6561f015cb34ab9cc57
peter nadel <peter.m.nadel@gmail.com>
2023-07-06 10:25:21 -0400

    removed mps

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ st.title('Tufts Physical Therapy AI Tutor')
 @st.cache_resource
 def prep_model():
     model_name = "sentence-transformers/all-mpnet-base-v2"
-    model_kwargs = {'device': 'mps'}
+    # model_kwargs = {'device': 'mps'}
     encode_kwargs = {'normalize_embeddings': False}
     hf = HuggingFaceEmbeddings(
         model_name=model_name,
-        model_kwargs=model_kwargs,
+        # model_kwargs=model_kwargs,
         encode_kwargs=encode_kwargs
     )
     db = Chroma(persist_directory='./p_db', embedding_function=hf)
